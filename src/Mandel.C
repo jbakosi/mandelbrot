a2b07a3457beba4ecbcea8725ee85da30bb7a5c0
Jozsef Bakosi <jbakosi@lanl.gov>
2017-08-26 12:18:57 -0600

    Move mandelbrot_fn inside mandelChare

--- src/Mandel.C
+++ src/Mandel.C
@@ -33,59 +33,6 @@ uint64_t numChare;
   #pragma clang diagnostic pop
 #endif
 
-using namespace boost::gil;
-
-// Models a Unary Function
-template <typename P>   // Models PixelValueConcept
-struct mandelbrot_fn {
-    using point_t = point2< ptrdiff_t >;
-
-    using const_t = mandelbrot_fn;
-    using value_type = P;
-    using reference = value_type;
-    using const_reference = value_type;
-    using argument_type = point_t;
-    using result_type = reference;
-    BOOST_STATIC_CONSTANT(bool, is_mutable=false);
-
-    value_type                    _in_color,_out_color;
-    point_t                       _img_size;
-    static const int MAX_ITER=100;        // max number of iterations
-
-    mandelbrot_fn() {}
-    mandelbrot_fn( const point_t& sz,
-                   const value_type& in_color,
-                   const value_type& out_color ) :
-      _in_color(in_color), _out_color(out_color), _img_size(sz) {}
-
-    result_type operator()(const point_t& p) const {
-        // normalize the coords to (-2..1, -1.5..1.5)
-        // (actually make y -1.0..2 so it is asymmetric, so we can verify some
-        // view factory methods)
-        double t = get_num_iter(
-          point2<double>( p.x/static_cast<double>(_img_size.x)*3.0-2,
-                          p.y/static_cast<double>(_img_size.y)*3.0-1.5 ) );
-        t = std::pow( t, 0.2 );
-
-        value_type ret;
-        for (std::size_t k=0; k<num_channels<P>::value; ++k)
-            ret[k] = static_cast< typename channel_type<P>::type >(
-                     (_in_color[k]*t + _out_color[k]*(1-t)) );
-        return ret;
-    }
-
-private:
-    double get_num_iter(const point2<double>& p) const {
-        point2<double> Z(0,0);
-        for (int i=0; i<MAX_ITER; ++i) {
-            Z = point2<double>(Z.x*Z.x - Z.y*Z.y + p.x, 2*Z.x*Z.y + p.y);
-            if (Z.x*Z.x + Z.y*Z.y > 4)
-                return i/static_cast<double>(MAX_ITER);
-        }
-        return 0;
-    }
-};
-
 class Main : public CBase_Main {
 
   private:
@@ -229,14 +176,6 @@ class Main : public CBase_Main {
       // create the chareArray
       CProxy_mandelChare mandelArray =
         CProxy_mandelChare::ckNew(numChare,chunksize,remainder,deltax);
-
-      // --- part of the serial code
-      //point_t dims( numPixel, numPixel );
-      //my_virt_view_t mandel(dims, locator_t(point_t(0,0), point_t(1,1),
-      //  deref_t(dims, rgb8_pixel_t(255,0,255), rgb8_pixel_t(0,255,0))));
-      //jpeg_write_view("out-mandelbrot.jpg",mandel);
-
-      //CkExit();
     }
 
     // reduction to ensure completion and then exit
@@ -247,15 +186,69 @@ class Main : public CBase_Main {
       CkPrintf(" ------------------------------------------------------ \n \n");
       CkExit();
     }
-
-
-
 };
 
 // *****************************************************************************
 class mandelChare : public CBase_mandelChare
 {
         private:
+
+        // class that generates pixels of Mandelbrot set
+        template <typename P>   // Models PixelValueConcept
+        struct mandelbrot_fn {
+
+            template< typename T > using point2 = boost::gil::point2< T >;
+            using num_channels = typename boost::gil::num_channels< P >;
+            using channel_type = typename boost::gil::channel_type< P >::type;
+
+            using point_t = point2< ptrdiff_t >;
+
+            using const_t = mandelbrot_fn;
+            using value_type = P;
+            using reference = value_type;
+            using const_reference = value_type;
+            using argument_type = point_t;
+            using result_type = reference;
+            BOOST_STATIC_CONSTANT(bool, is_mutable=false);
+
+            value_type                    _in_color,_out_color;
+            point_t                       _img_size;
+            static const int MAX_ITER=100;        // max number of iterations
+
+            mandelbrot_fn() {}
+            mandelbrot_fn( const point_t& sz,
+                           const value_type& in_color,
+                           const value_type& out_color ) :
+              _in_color(in_color), _out_color(out_color), _img_size(sz) {}
+
+            result_type operator()(const point_t& p) const {
+
+                // normalize the coords to (-2..1, -1.5..1.5)
+                // (actually make y -1.0..2 so it is asymmetric, so we can verify some
+                // view factory methods)
+                double t = get_num_iter(
+                  point2<double>( p.x/static_cast<double>(_img_size.x)*3.0-2,
+                                  p.y/static_cast<double>(_img_size.y)*3.0-1.5 ) );
+                t = std::pow( t, 0.2 );
+
+                value_type ret;
+                for (std::size_t k=0; k<num_channels::value; ++k)
+                    ret[k] = static_cast< channel_type >(
+                             (_in_color[k]*t + _out_color[k]*(1-t)) );
+                return ret;
+            }
+
+            private:
+            double get_num_iter(const point2<double>& p) const {
+                point2<double> Z(0,0);
+                for (int i=0; i<MAX_ITER; ++i) {
+                    Z = point2<double>(Z.x*Z.x - Z.y*Z.y + p.x, 2*Z.x*Z.y + p.y);
+                    if (Z.x*Z.x + Z.y*Z.y > 4)
+                        return i/static_cast<double>(MAX_ITER);
+                }
+                return 0;
+            }
+        };
 
         public:
 
@@ -269,7 +262,7 @@ class mandelChare : public CBase_mandelChare
                 double xbeg, xend;
                 int width;
 
-                xbeg = (double)thisIndex     * deltax;
+                xbeg = (double)thisIndex * deltax;
 
                 if (thisIndex == numChare)
                 {
@@ -284,13 +277,15 @@ class mandelChare : public CBase_mandelChare
 
                 //CkPrintf("%d : %d : %f , %f \n ",thisIndex,width,xbeg,xend);
 
+                using rgb8_pixel_t = boost::gil::rgb8_pixel_t;
                 using deref_t = mandelbrot_fn< rgb8_pixel_t >;
                 using point_t = deref_t::point_t;
-                using locator_t = virtual_2d_locator< deref_t, false >;
-                using my_virt_view_t = image_view< locator_t >;
+                using locator_t = boost::gil::virtual_2d_locator< deref_t, false >;
+                using my_virt_view_t = boost::gil::image_view< locator_t >;
 
-                boost::function_requires< PixelLocatorConcept<locator_t> >();
-                gil_function_requires< StepIteratorConcept< locator_t::x_iterator > >();
+                boost::function_requires< boost::gil::PixelLocatorConcept<locator_t> >();
+                boost::gil::gil_function_requires<
+                  boost::gil::StepIteratorConcept< locator_t::x_iterator > >();
 
                 point_t dims( width, numPixel );
                 my_virt_view_t mandel(dims, locator_t(point_t(xbeg,0), point_t(xend,1),
